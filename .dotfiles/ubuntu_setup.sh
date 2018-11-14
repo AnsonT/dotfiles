b52934425550075ec8dc3ca81b52050fec25a0d3
AnsonT <tsaoa@acm.org>
2018-11-13 16:20:47 -0800

    Update ubuntu_setup.sh

--- .dotfiles/ubuntu_setup.sh
+++ .dotfiles/ubuntu_setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install build-essential
+sudo apt-get install build-essential cmake
 
 wget https://github.com/sharkdp/bat/releases/download/v0.9.0/bat_0.9.0_amd64.deb
 dpkg -i bat_0.9.0_amd64.deb
