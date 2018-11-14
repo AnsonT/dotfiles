9854d31a5372010dc21f2b4619e333e9e755ff16
AnsonT <tsaoa@acm.org>
2018-11-13 16:15:07 -0800

    Create ubuntu_setup.sh

---
+++ .dotfiles/ubuntu_setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install build-essential
+
+wget https://github.com/sharkdp/bat/releases/download/v0.9.0/bat_0.9.0_amd64.deb
+dpkg -i bat_0.9.0_amd64.deb
+
+curl -sSf https://static.rust-lang.org/rustup.sh | sh
+cargo intall exa
