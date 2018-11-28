4ede390e4deaa965a8d7327ee6b76805baf587ed
Anson Tsao <tsaoa@acm.org>
2018-11-28 08:18:15 +0000

    fixed install spelling error

--- .dotfiles/ubuntu_setup.sh
+++ .dotfiles/ubuntu_setup.sh
@@ -4,4 +4,4 @@ wget https://github.com/sharkdp/bat/releases/download/v0.9.0/bat_0.9.0_amd64.deb
 dpkg -i bat_0.9.0_amd64.deb
 
 curl -sSf https://static.rust-lang.org/rustup.sh | sh
-cargo intall exa
+cargo install exa
