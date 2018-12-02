6bf06ffb66f4c8fdb048c3db2d17a57ffc598b8b
Anson Tsao <ansont@macos-10.13.6.shared>
2018-12-02 09:21:50 -0800

    added lang_setup

--- .bashrc
+++ .bashrc
@@ -7,5 +7,4 @@ export PATH="$PATH:$HOME/.cargo/bin"
 [ -r ~/.dotfiles/aliases ] && . ~/.dotfiles/aliases
 [ -r ~/.bash_prompt ] && . ~/.bash_prompt
 [ -r ~/.bin/z.sh ] && . ~/.bin/z.sh
-
-
+[ -r /usr/local/opt/asdf/asdf.sh ] && source /usr/local/opt/asdf/asdf.sh
---
+++ .dotfiles/lang_setup.sh
@@ -0,0 +1,13 @@
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf install nodejs 11.3.0
+asdf install nodejs 10.14.1
+asdf install nodejs 8.14.0
+asdf global nodejs 11.3.0
+
+asdf install python 3.7.1
+asdf install python 2.7.15
+asdf global python 3.7.1 2.7.15
+
+asdf install golang 1.11
+asdf global golang 1.11
+
