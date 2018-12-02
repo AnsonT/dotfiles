c52377be488f71fbaf7942439269b54790201c31
Anson Tsao <tsaoa@acm.org>
2018-12-02 09:27:26 -0800

    added plugins for lang_setup

--- .dotfiles/lang_setup.sh
+++ .dotfiles/lang_setup.sh
@@ -1,13 +1,16 @@
+asdf plugin-add nodejs
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 asdf install nodejs 11.3.0
 asdf install nodejs 10.14.1
 asdf install nodejs 8.14.0
 asdf global nodejs 11.3.0
 
+asdf plugin-add python
 asdf install python 3.7.1
 asdf install python 2.7.15
 asdf global python 3.7.1 2.7.15
 
+asdf plugin-add golang
 asdf install golang 1.11
 asdf global golang 1.11
 
